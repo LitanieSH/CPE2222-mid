6358dd6f9036699de18a61eae6b580191215293a
LitanieSH <shinhye_live@msn.com>
2023-03-19 23:30:18 +0700

    as4

--- as4.py
+++ as4.py
@@ -1,6 +1,6 @@
 AA=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 aa=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-Aa = AA+aa
+AAaa = AA+aa
 
 s=str(input("your code :"))
 n1=int(input("your num1 :"))
@@ -12,7 +12,7 @@ se.extend(s)
 SN = []
 for x in range (0,len(se)):
     if (x%2) == 0 :
-        check = Aa.index(se[x])
+        check = AAaa.index(se[x])
         if check < 26:
             num = AA.index(se[x])
             num = ((num+n1)%26)
@@ -22,7 +22,7 @@ for x in range (0,len(se)):
             num = ((num+n1)%26)
             SN.append(aa[num])
     else:
-        check = Aa.index(se[x])
+        check = AAaa.index(se[x])
         if check < 26:
             num = AA.index(se[x])
             num = ((num+n2)%26)

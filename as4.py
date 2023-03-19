f952eb9da9e9c4b7050d5de4de3f55bc65147a59
LitanieSH <shinhye_live@msn.com>
2023-03-19 23:29:06 +0700

    as4done

--- as4.py
+++ as4.py
@@ -1,6 +1,6 @@
 AA=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 aa=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-all = AA+aa
+Aa = AA+aa
 
 s=str(input("your code :"))
 n1=int(input("your num1 :"))
@@ -12,17 +12,25 @@ se.extend(s)
 SN = []
 for x in range (0,len(se)):
     if (x%2) == 0 :
-        num = all.index(se[x])
-        num= num+n1
-        if num > 51:
-            num= num-52
-        SN.append(all[num])    
+        check = Aa.index(se[x])
+        if check < 26:
+            num = AA.index(se[x])
+            num = ((num+n1)%26)
+            SN.append(AA[num])
+        elif check >= 26:
+            num = aa.index(se[x])
+            num = ((num+n1)%26)
+            SN.append(aa[num])
     else:
-        num = all.index(se[x])
-        num = num+n2
-        if num > 51:
-            num = num-52
-        SN.append(all[num])
+        check = Aa.index(se[x])
+        if check < 26:
+            num = AA.index(se[x])
+            num = ((num+n2)%26)
+            SN.append(AA[num])
+        elif check >= 26:
+            num = aa.index(se[x])
+            num = ((num+n2)%26)
+            SN.append(aa[num])
 
 SN = ''.join(SN)
 
